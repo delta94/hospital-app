bb7907e336c37cc9ea5f4dfcf4a7716205ae4fae
rashedInt32 <parvez08eee@gmail.com>
2019-09-29 11:40:35 +0600

    Utility component

---
+++ src/components/ui/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Button = ({text}) =>
+  <button
+    className="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+  >
+    {text}
+  </button>
+
+export default Button;
--- src/pages/superadmin/CreateHospital.js
+++ src/pages/superadmin/CreateHospital.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Layout from '../../hoc/Layout';
 import Input from '../../components/forms/Input';
+import Button from '../../components/ui/Button';
 
 function CreateHospital() {
   const [hospital, setHospital] = useState({
@@ -11,19 +12,25 @@ function CreateHospital() {
       <h2 className="pb-3">Hospitals</h2>
 
       <form action="">
-        <Input
-          label="Create Hospital"
-          name="name"
-          value={hospital.name}
-          onChange={(e) => setHospital({name: e.target.value})}
-          placeholder="Hospital Name"
-          className="form-control"
-        />
-
+        <div className="row align-items-end">
+          <div className="col-md-8">
+            <Input
+              label="Create Hospital"
+              name="name"
+              bm={false}
+              value={hospital.name}
+              onChange={e => setHospital({ name: e.target.value })}
+              placeholder="Hospital Name"
+              className="form-control mb-0"
+            />
+          </div>
+          <div className="col-md-4">
+            <Button type="submit" text="Add Hospital" />
+          </div>
+        </div>
       </form>
     </Layout>
-  )
-
-}
+  );
+};
 
 export default CreateHospital;

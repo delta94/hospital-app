670315311101727b9585723c29a8bfacb34a7427
rashedInt32 <parvez08eee@gmail.com>
2019-10-09 00:23:06 +0600

    some destructering

--- src/pages/HospitalProfile.js
+++ src/pages/HospitalProfile.js
@@ -1,45 +1,50 @@
-import React, {useContext} from 'react';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import to from 'await-to-js';
+import React, { useContext } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import to from "await-to-js";
 
-import { config } from '../config';
-import { AuthContext } from '../context/authContext';
+import { config } from "../config";
+import { AuthContext } from "../context/authContext";
 
-import { SINGLE_HOSPITAL } from '../graphql/Query';
+import { SINGLE_HOSPITAL } from "../graphql/Query";
 import { UPLOAD_FILE } from "../graphql/Mutation";
-import { HOSPITAL_UPDATE_MUTATION } from '../graphql/Mutation';
+import { HOSPITAL_UPDATE_MUTATION } from "../graphql/Mutation";
 
-import Layout from '../hoc/Layout';
-import Loader from '../components/ui/Loader';
-import Button from '../components/ui/Button';
-import FileUpload from '../components/forms/FileUpload';
+import Layout from "../hoc/Layout";
+import Loader from "../components/ui/Loader";
+import Button from "../components/ui/Button";
+import FileUpload from "../components/forms/FileUpload";
 
 //import img from '../img/image-placeholder.jpg';
-
 
 function Hospital() {
   const { user } = useContext(AuthContext);
 
-  const { loading, data} = useQuery(SINGLE_HOSPITAL, {
-    variables: {id: user.hospital}
+  const { loading, data } = useQuery(SINGLE_HOSPITAL, {
+    variables: { id: user.hospital }
   });
 
   const [singleUpload] = useMutation(UPLOAD_FILE);
   const [updateHospital] = useMutation(HOSPITAL_UPDATE_MUTATION, {
-    refetchQueries: [{ query: SINGLE_HOSPITAL, variables: {id: user.hospital} }]
+    refetchQueries: [
+      { query: SINGLE_HOSPITAL, variables: { id: user.hospital } }
+    ]
   });
 
-
-  const handleFile = async (e) => {
+  const handleFile = async e => {
     const [file] = e.target.files;
     // Upload mutation for get the
-    const [, response] = await to(singleUpload({
-      variables: {
-        file,
-        id: data.hospital.id,
-        type: e.target.name
-      }
-    }));
+    const [, response] = await to(
+      singleUpload({
+        variables: {
+          file,
+          id: data.hospital.id,
+          type: e.target.name
+        }
+      })
+    );
+
+    const { data: { singleUpload: { filename } } } = response;
+    const filePath = config.staticUrl + filename
 
     await to(
       updateHospital({
@@ -47,7 +52,7 @@ function Hospital() {
           id: data.hospital.id,
           update: {
             name: data.hospital.name,
-            coverphoto: config.staticUrl + response.data.singleUpload.filename
+            coverphoto: filePath
           }
         }
       })
@@ -55,31 +60,35 @@ function Hospital() {
   };
 
   if (loading)
-    return <Layout><Loader /></Layout>;
+    return (
+      <Layout>
+        <Loader />
+      </Layout>
+    );
 
   return (
     <Layout>
       <div className="card bg-white text-white">
         <div
           className="overlay-img bg-primary text-right p-2"
-          style={{ background: "url("+ data.hospital.coverphoto + ") center center no-repeat" }}
+          style={{
+            background:
+              "url(" + data.hospital.coverphoto + ") center center no-repeat"
+          }}
         >
-          {user.role === 'admin' ? <Button text="Edit" btnSize="sm"/> : null}
+          {user.role === "admin" ? <Button text="Edit" btnSize="sm" /> : null}
           <div className="card-img-overlay">
             <FileUpload onChange={handleFile} />
           </div>
         </div>
 
-
-        <h2 className="fc-white">{data.hospital.name}</h2>
-        <p className="card-text">{data.hospital.location}</p>
-
+        <div className="card-body">
+          <h2 className="fc-white">{data.hospital.name}</h2>
+          <p className="card-text">{data.hospital.location}</p>
+        </div>
       </div>
-
-
-
     </Layout>
   );
-};
+}
 
 export default Hospital;

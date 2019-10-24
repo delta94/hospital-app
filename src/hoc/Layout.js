6abb4a7036044f14e23dfd8cdad9c010d036838a
rashedInt32 <parvez08eee@gmail.com>
2019-10-24 11:22:13 +0600

    some refactor

--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Loader from '../components/ui/Loader';
 
-const Layout = ({children, loading, auth}) => {
+const Layout = ({children, loading}) => {
   return (
     <div className="container-fluid page-body-wrapper">
       <div className="main-panel">
-        <div className={!auth ? 'content-wrapper' : ''}>
+        <div className="content-wrapper">
           {loading ? <Loader /> : children}
         </div>
       </div>

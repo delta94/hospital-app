ddbf78fea0c153719eed82ef31bed637ffa34cc8
rashedInt32 <parvez08eee@gmail.com>
2019-10-26 16:46:05 +0600

    admin user table

--- src/pages/admin/HospitalUsers.js
+++ src/pages/admin/HospitalUsers.js
@@ -1,8 +1,12 @@
 import React, { useContext } from "react";
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { omit } from "lodash";
+import to from "await-to-js";
+
+import { HOSPITAL_USERS } from "../../graphql/Query";
+import { UPDATE_USER_MUTATION } from "../../graphql/Mutation";
 
 import Loader from "../../components/ui/Loader";
-import { HOSPITAL_USERS } from "../../graphql/Query";
 import { AuthContext } from "../../context/authContext";
 import HospitalUserTable from "../../components/table/hosiptalUserTable";
 
@@ -10,8 +14,30 @@ const HospitalUsers = () => {
   const { user } = useContext(AuthContext);
 
   const { loading, data } = useQuery(HOSPITAL_USERS, {
+    fetchPolicy: "cache-and-network",
     variables: { id: user.hospital }
   });
+
+
+  const [updateUser] = useMutation(UPDATE_USER_MUTATION);
+
+  const approvedUser = async (user) => {
+    user = omit(user, ['__typename']);
+    const [err] = await to(
+      updateUser({
+        variables: { userInput: { ...user, pending: !user.pending } },
+        refetchQueries: [
+          {
+            query: HOSPITAL_USERS,
+            variables: { id: user.hospital },
+            fetchPolicy: "cache-and-network"
+          }
+        ]
+      })
+    );
+
+    console.log(err);
+  };
 
 
   if (loading) return <Loader />;
@@ -20,7 +46,28 @@ const HospitalUsers = () => {
     <div className="card">
       <div className="card-body">
         <h4 className="card-name">Users Info</h4>
-        <HospitalUserTable users={data.getHospitalUsers} />
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th>First name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.getHospitalUsers.map(user => <HospitalUserTable
+              key={user.id}
+              firstName={user.firstName}
+              lastName={user.lastName}
+              avatar={user.avatar}
+              role={user.role}
+              onApprovedUser={() => approvedUser(user)}
+             />)}
+          </tbody>
+        </table>
       </div>
     </div>
   );
